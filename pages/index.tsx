e5c3e934f902bd85fe7772435f2b87f11ced37f1
Name <you@example.com>
2023-06-27 12:52:01 +0700

    free2

--- pages/index.tsx
+++ pages/index.tsx
@@ -360,11 +360,11 @@ const Decreepies: NextPage = () => {
             </p>
             <p className={styles.wenStealthLaunch}>
               <span className={styles.wen}>Price?</span>
-              <span> First 500 free FCFS, then 0.0013 ETH</span>
+              <span> First 500 Free, then 0.0013 ETH</span>
             </p>
             <p className={styles.wenStealthLaunch}>
               <span className={styles.wen}>Max Wallet?</span>
-              <span> 2 (free) / 20 (0.0013) per wallet</span>
+              <span> 2 (free) / 20 (pay) per wallet</span>
             </p>
           </div>
         </div>
